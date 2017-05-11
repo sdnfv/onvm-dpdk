f8f01b20fe3ebc682e367972d2d5e9e1a4ddbbd3
Nick Yurchenko <urchennko@gmail.com>
2017-05-11 16:11:21 -0400

    Changed setup_iface script for dpdk interface
    - Changed args to include interface name, ip, netmask, up/down

---
+++ tools/dpdk-setup-iface.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# check if there is only one additional command-line argument
+if [ $# -ne 4 ]
+then
+    echo "This script is used to setup ifconfig interfaces for dpdk"
+    echo "Usage:"
+    echo "sudo $0 <interface name> <ip address> <netmask> <up/down>"
+    exit 1
+fi
+
+# Check if you are root
+user=`whoami`
+if [ "root" != "$user" ]
+then
+    echo "You are not root!"
+    exit 1
+fi
+
+# Create & configure /dev/dpdk-iface
+rm -rf /dev/dpdk-iface
+mknod /dev/dpdk-iface c 1110 0
+chmod 666 /dev/dpdk-iface
+
+# First check whether igb_uio module is already loaded
+MODULE="igb_uio"
+
+if lsmod | grep "$MODULE" &> /dev/null ; then
+  echo "$MODULE is loaded!"
+else
+  echo "$MODULE is not loaded!"
+  exit 1
+fi
+
+echo "/sbin/ifconfig $1 $2 netmask $3 $4"
+/sbin/ifconfig $1 $2 netmask $3 $4
